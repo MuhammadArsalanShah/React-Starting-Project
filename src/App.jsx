fbdda24cc25cbbfdd9e6301ec98a59e24122fbc3
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-08-21 17:26:11 +0500

    Using props for dynamic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,63 @@
-// import reactImg from "./assets/react-core-concepts.png";
+import reactImg from "./assets/react-core-concepts.png";
+import componentImg from "./assets/components.png";
 
-// console.log('this is img: ',reactImg);
+console.log('this is img: ',reactImg);
 
-// const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
-// function genRandomNum(max) {
-//   return Math.floor(Math.random() * (max + 1));
-// }
-// function Header() {
-//   const description = reactDescriptions[genRandomNum(reactDescriptions.length - 1)];
+function genRandomNum(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
 
-//   return (
-//     <header>
-//       <img src={reactImg} alt="Stylized atom" />
-//       <h1>React Essentials</h1>
-//       <p>
-//         {description} React concepts you will need for almost any app you are
-//         going to build!
-//       </p>
-//     </header>
-//   );
-// }
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );  
+}
 
-// function App() {
-//   return (
-//     <div>
-//       <Header />
-//       <main>
-//         <h2>Time to get started!</h2>
-//       </main>
-//     </div>
-//   );
-// }
+function Header() {
+  const description = reactDescriptions[genRandomNum(reactDescriptions.length - 1)];
 
-// export default App;
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="Components" 
+              description="The core UI building block" 
+              image={componentImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
 
 
 
@@ -45,7 +69,7 @@
 
 
 /**** Coding Exercise 4 ****/
-
+/*
 import React from 'react';
 
 export const userData = {
@@ -79,3 +103,4 @@ function App() {
 }
 
 export default App;
+*/

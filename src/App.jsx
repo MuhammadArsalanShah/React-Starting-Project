daec883434f82093397a21e94904081e28d7359c
MuhammadArsalanShah <arsalanshah408@yahoo.com>
2024-08-21 17:45:23 +0500

    Alternative and shorter syntaxes for props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
-import componentImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
 
 console.log('this is img: ',reactImg);
 
@@ -9,12 +9,22 @@ function genRandomNum(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function CoreConcept(props) {
+// function CoreConcept(props) {
+//   return (
+//     <li>
+//       <img src={props.image} alt={props.title} />
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
+//     </li>
+//   );  
+// }
+
+function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );  
 }
@@ -42,14 +52,14 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept 
-              title="Components" 
-              description="The core UI building block" 
-              image={componentImg}
+              title={CORE_CONCEPTS[3].title} 
+              description={CORE_CONCEPTS[3].description} 
+              image={CORE_CONCEPTS[3].image}
             />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
           </ul>
         </section>
       </main>
